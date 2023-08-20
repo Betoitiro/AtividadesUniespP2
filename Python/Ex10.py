420c83d6778e04d65a81c9634353998919581561
= <=>
2023-08-20 10:53:09 -0300

    correção de codigo

--- Python/Ex10.py
+++ Python/Ex10.py
@@ -1,37 +1,37 @@
 import random
 
-def get_user_choice():
+def JogadaUsuario():
     while True:
-        user_choice = input("Escolha Pedra, Papel ou Tesoura: ").lower()
-        if user_choice in ["pedra", "papel", "tesoura"]:
-            return user_choice
+        Jogador = input("Escolha Pedra, Papel ou Tesoura: ").lower()
+        if Jogador in ["pedra", "papel", "tesoura"]:
+            return Jogador
         else:
-            print("Escolha inválida. Por favor, escolha entre Pedra, Papel ou Tesoura.")
+            print("Escolha inválida")
 
-def get_computer_choice():
+def JogoPC():
     choices = ["pedra", "papel", "tesoura"]
     return random.choice(choices)
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
+def computador(Jogador, computador):
+    if Jogador == computador:
         return "Empate"
-    elif (user_choice == "pedra" and computer_choice == "tesoura") or \
-         (user_choice == "papel" and computer_choice == "pedra") or \
-         (user_choice == "tesoura" and computer_choice == "papel"):
+    elif (Jogador == "pedra" and computador == "tesoura") or \
+         (Jogador == "papel" and computador == "pedra") or \
+         (Jogador == "tesoura" and computador == "papel"):
         return "Você venceu!"
     else:
         return "O computador venceu!"
 
-def main():
+def chefe():
     print("Bem-vindo ao Pedra, Papel e Tesoura!")
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
+    Jogador = JogadaUsuario()
+    computador = JogoPC()
 
-    print(f"Você escolheu: {user_choice}")
-    print(f"O computador escolheu: {computer_choice}")
+    print(f"Você escolheu: {Jogador}")
+    print(f"O computador escolheu: {computador}")
 
-    winner = determine_winner(user_choice, computer_choice)
+    winner = computador(Jogador, computador)
     print(winner)
 
-if __name__ == "__main__":
-    main()
+if __name__ == "xfede":
+    chefe()
