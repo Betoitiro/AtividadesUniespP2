4dccd06ae23eb091059b065a1be14ef88c1e0509
= <=>
2023-08-28 19:49:55 -0300

    atividade de sala

---
+++ JAVA/Aula2/AppPessoa.java
@@ -0,0 +1,9 @@
+package JAVA.Aula2;
+
+public class AppPessoa {
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa();
+        p1.niver();
+    }
+}
---
+++ JAVA/Aula2/Pessoa.java
@@ -0,0 +1,10 @@
+package JAVA.Aula2;
+
+public class Pessoa {
+    int idade = 18;
+
+    public void niver(){
+        idade ++;
+        System.out.println(idade);
+    }
+}
