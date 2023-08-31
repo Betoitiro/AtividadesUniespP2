167b8975b40feecdc6de085e821c77dd96d027ef
= <=>
2023-08-31 17:47:54 -0300

    cod the active

--- Front/atividade1/Calculadora.js
+++ Front/atividade1/Calculadora.js
@@ -1,38 +1,9 @@
-const readline = require('readline');
+var nota1 = parseFloat(prompt("Digite a primeira nota:"));
+var nota2 = parseFloat(prompt("Digite a segunda nota:"));
+var nota3 = parseFloat(prompt("Digite a terceira nota:"));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-function calculate(num1, operator, num2) {
-  num1 = parseFloat(num1);
-  num2 = parseFloat(num2);
 
-  switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    case '/':
-      return num1 / num2;
-    default:
-      return 'Operador inválido';
-  }
-}
+var mediaPonderada = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / (2 + 3 + 5);
 
-function startCalculator() {
-  rl.question('Digite o primeiro número: ', num1 => {
-    rl.question('Digite o operador (+, -, *, /): ', operator => {
-      rl.question('Digite o segundo número: ', num2 => {
-        const result = calculate(num1, operator, num2);
-        console.log(`Resultado: ${result}`);
-        rl.close();
-      });
-    });
-  });
-}
-
-startCalculator();
+alert("A média ponderada é: " + mediaPonderada.toFixed(2));
