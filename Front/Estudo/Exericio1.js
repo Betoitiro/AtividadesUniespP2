e45a9667a587cfa0e149caf718e22c348e7e0f59
= <=>
2023-09-01 00:03:03 -0300

    exercice one

---
+++ Front/Estudo/Exericio1.js
@@ -0,0 +1,12 @@
+alert("Indentificador de idade: ");
+
+var ano = parseInt(prompt("Digite o ano que voce nasceu: "));
+var idade = parseInt(2023 - ano);
+if(idade >=18 ){
+    alert("Acesso permitido! \n o usuario ja é maior de idade")
+    alert("voce tem "+ idade + " anos de idade");
+}else{
+    alert("Acesso negado!! \n usuario ainda é de menor")
+    alert("voce tem "+ idade + " anos de idade");
+}
+
