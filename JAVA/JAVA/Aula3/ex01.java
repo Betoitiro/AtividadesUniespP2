870c872fdcdeba7e25e70df0b1aead6308c3445d
= <=>
2023-09-05 08:28:23 -0300

    aula

---
+++ JAVA/JAVA/Aula3/ex01.java
@@ -0,0 +1,12 @@
+package JAVA.Aula3;
+
+
+public class ex01 {
+
+    public static void main(String[] args) {
+        
+        ex01Metodo e1 = new ex01Metodo(89, 1.81);
+        e1.IMC();
+
+    }
+}
