7157da6e225114fabf20166c416965af8a7dad6b
= <=>
2023-09-11 23:02:25 -0300

    nota de aula

--- JAVA/Aula4/NotaEx02/App.java
+++ JAVA/Aula4/NotaEx02/App.java
@@ -7,22 +7,37 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("1: Diretor\n2:Gerente");
-        String cargo = sc.nextLine();
+        int cargo = sc.nextInt();
 
         switch (cargo) {
             case 1:
                 System.out.println("Nome: ");
                 String nome = sc.nextLine();
                 sc.nextLine();
+                
                 System.out.println("Salario: ");
                 double salario = sc.nextDouble();
+                
                 System.out.println("Quantidade de ações: ");
                 int acoes = sc.nextInt();
 
                 Diretor d1 = new Diretor(nome, salario, acoes);
                 d1.ExibiDados();
                 break;
-        
+            case 2:
+                System.out.println("Nome: ");
+                String nomeG = sc.nextLine();
+                sc.nextLine();
+                System.out.println("Salario: ");
+                double SalarioG = sc.nextDouble();
+                sc.nextLine();
+                System.out.println("Departamento: ");
+                String dep = sc.nextLine();
+
+                Gerente g1 = new Gerente(nomeG, SalarioG, dep);
+                g1.AjusteSalarial();
+
+                break;
             default:
                 break;
         }
