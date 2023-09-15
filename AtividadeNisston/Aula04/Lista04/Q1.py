a0183888b33ea320cb4b4237390076ed75d548c6
= <=>
2023-09-15 19:55:03 -0300

    organizando o codigo

--- AtividadeNisston/Aula04/Lista04/Q1.py
+++ AtividadeNisston/Aula04/Lista04/Q1.py
@@ -1,11 +1,18 @@
+
+
+def selecao_Ordem(vetor):
+    n = len(Vetor)
+    #crirar uma variavel para que leia a quantidade de elementos que tenham dentro do vetor
+
+    for i in range(n):  
+        indiceM = i
+        for j in range(i + 1, n):
+            if vetor[j] < vetor[indiceM]:
+                indiceM = j
+        vetor[i], vetor[indiceM] = vetor[indiceM], vetor[i]
+
+
 Vetor = [5, 4, 7, 0, 1, 2, 8, 9, 10, 3, 6]
 #criar um vetor com numeros de maneira desordenadas-
-n = len(Vetor)
-#crirar uma variavel para que leia a quantidade de elementos que tenham dentro do vetor
-
-for i in range(n - 1):  #loop for usando a quantidade de elementos -1 para o sistema execulte a quantidade de vezes que atribua todos os elementos
-    for j in range(0, n - i - 1): #segundo for para que diminua a quantidaded e vezes que o programa vai se repetir 
-        if Vetor[j] > Vetor[j + 1]: #conficção caso J seja maior que J + 1
-            # variavel para que possa udar a sequencia do
-            Vetor[j], Vetor[j + 1] = Vetor[j + 1], Vetor[j]
+selecao_Ordem(Vetor)
 print(Vetor)
