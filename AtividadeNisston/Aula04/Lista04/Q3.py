841ab45a9ed4f477120a675cb7d7feae1eb869c9
= <=>
2023-09-15 20:18:24 -0300

    atividade de sala

--- AtividadeNisston/Aula04/Lista04/Q3.py
+++ AtividadeNisston/Aula04/Lista04/Q3.py
@@ -15,8 +15,19 @@ def encontrar_Min(vetor):
             minimo = elemeneto
     return minimo
 
+def encontrar_max_Min(vetor):
+    maximo = vetor[0]
+    minimo = vetor[0]
+    
+    for elemeneto in vetor:
+        if elemeneto > maximo:
+            maximo = elemeneto
+        if elemeneto < minimo:
+            minimo = elemeneto
+    return maximo, minimo
+
 vetor = [1 ,3,5,6,8,7,6,5,4]
-print(encontrar_Max(vetor))
-print(encontrar_Min(vetor))
-#rep = encontrar_Max(vetor)
-#print(rep[0], rep[1])
+#print(encontrar_Max(vetor))
+#print(encontrar_Min(vetor))
+rep = encontrar_max_Min(vetor)
+print(rep[0], rep[1])
