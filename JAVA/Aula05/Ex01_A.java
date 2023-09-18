f717ed57d3706fd60880dd2000f5b88ebc600176
= <=>
2023-09-18 20:07:47 -0300

    atividade 01 sala

---
+++ JAVA/Aula05/Ex01_A.java
@@ -0,0 +1,57 @@
+package Aula05;
+
+public class Ex01_A {
+    
+    private String nome;
+    private int idade;
+    private int matricula;
+
+
+    
+    public Ex01_A() {
+    }
+
+
+    public Ex01_A(String nome, int idade, int matricula) {
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public void Status(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Matricula "+ matricula);
+    }
+
+}
