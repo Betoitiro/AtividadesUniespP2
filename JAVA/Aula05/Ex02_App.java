85616ed07009d0e916bcd2cb73b61de68b88b0db
= <=>
2023-09-18 20:17:31 -0300

    exercicio 2

---
+++ JAVA/Aula05/Ex02_App.java
@@ -0,0 +1,12 @@
+package Aula05;
+
+public class Ex02_App {
+    public static void main(String[] args) {
+        
+        Ex02 p1 = new Ex02("camisa", 100.0, "Polo branca");
+        Ex02 p2 = new Ex02("Calça", 50.0, "Calça azul");
+
+        p1.Status();
+        p2.Status();
+    }
+}
