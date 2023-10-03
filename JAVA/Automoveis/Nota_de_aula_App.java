c4a3230ac5bceb060716013b62bb3fba2dcb1076
= <=>
2023-10-02 22:29:50 -0300

    Atividades_De_Java

---
+++ JAVA/Automoveis/Nota_de_aula_App.java
@@ -0,0 +1,10 @@
+package Automoveis;
+
+public class Nota_de_aula_App {
+    
+    public static void main(String[] args) {
+        
+        Nota_de_aula a1 = new Nota_de_aula("Toyota", "Hillux");
+        a1.Status();
+    }
+}
---
+++ JAVA/IMC/ex01Metodo.java
@@ -0,0 +1,38 @@
+package IMC;
+
+public class ex01Metodo {
+
+    private double peso;
+    private double altura;
+    
+    public ex01Metodo(double peso, double altura) {
+		this.peso = peso;
+		this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void IMC(){
+        double imc = peso/(altura*altura);
+        System.out.println("O imc do individuo é de: " + imc);    
+    }
+
+    
+    
+    
+    
+}
