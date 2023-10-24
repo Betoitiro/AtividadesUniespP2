38e1d5ac405cb8e992d7a6e494a61a45d2a6ab31
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-10-23 22:00:14 -0300

    end the ropository

--- JAVA/Aula2/ex1/AppPessoa.java
+++
@@ -1,9 +0,0 @@
-package Aula2.ex1;
-
-public class AppPessoa {
-    public static void main(String[] args) {
-        
-        Pessoa p1 = new Pessoa();
-        p1.niver();
-    }
-}
--- JAVA/Project_Durante_Aula/index.java
+++
@@ -1,9 +0,0 @@
-package Project_Durante_Aula;
-
-public class index {
-
-    public static void main(String[] args) {
-        
-        
-    }
-}
